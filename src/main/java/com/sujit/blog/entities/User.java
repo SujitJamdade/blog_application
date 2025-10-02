ee61634a99bbb5fa3b487a0f6cdc5f697d68f5be
SujitJamdade <jamdadesujit58@gmail.com>
2025-10-03 00:03:49 +0530

    Lombok annotation added

--- src/main/java/com/sujit/blog/entities/User.java
+++ src/main/java/com/sujit/blog/entities/User.java
@@ -8,10 +8,10 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name="users")
 public class User {
 
@@ -31,56 +31,4 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Post> post = new ArrayList<>();
 
-
-    // Constructor
-    public User() {}
-
-    public User(int id, String name, String email, String password, String about) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.about = about;
-    }
-
-    // Getters & Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
 }
